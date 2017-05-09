d950a61ba37cdaa04e3b651a13bb2ab9715d57ed
erdem koral <koral.erdem@gmail.com>
2017-05-08 23:04:43 -0700

    code review with Christy and Andrew

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # This my read me
+
+For portfolio5 assignment, Christy, Andrew and I worked together today.
+First I created a new javascript file (appView.js) to hold some of the functions related with page view.
+Second, fixed a minor bug in teaser function that triggered the read more/ read less feature on all of the articles when it is only needed to work on that specific article that triggered.
+We also corrected some indentation problems, tried make the code look bit more proper.
+
+
+Note:
+Unfortunately I started the portfolio assignment without reading the requirements, and assumed that portfolio5 assignment was about form building. Added some features for form building. Please disregard those when you review the code.
--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 var allChickens = [];
-var view ={};
 //object constructor
 function Chicken (chix)  {
   this.title = chix.title;
@@ -10,20 +9,23 @@ function Chicken (chix)  {
   this.publishedOn = chix.publishedOn;
   this.description = chix.description;
   this.image = chix.image;
+  this.toHtml = function() {
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    var template = $('#chickenTemplate').html();
+    var compiledTemplate = Handlebars.compile(template);
+    // $('#chickens').append(compiledTemplate(this));
+    return compiledTemplate(this);
+  };
 }
-
-Chicken.prototype.toHtml = function() {
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  var template = $('#chickenTemplate').html();
-  var compiledTemplate = Handlebars.compile(template);
-  // $('#chickens').append(compiledTemplate(this));
-  return compiledTemplate(this);
-
-};
 
 //passes constructed objects to allChickens
 rawChickenData.forEach(function(ele){
   allChickens.push(new Chicken(ele));
+});
+
+//sorts allChickens title alphabeticaly
+allChickens.sort(function(a,b){
+  return (a.title > b.title);
 });
 
 function populateChickenFilter (chickenName){
@@ -45,86 +47,3 @@ allChickens.forEach(function(a){
   populateChickenFilter(a.title);
   populateTemperFilter(a.temperament);
 });
-
-// filter by title
-view.chickFilter = function(){
-  $('.chicken-filter').change(function(){
-    $('article').hide();
-    var selectedValue = $(this).val();
-    console.log(selectedValue);
-    if(selectedValue){
-      $('article[data-name="' + selectedValue + '"]').fadeIn(800);
-    }else{
-      $('article').not('.template').show();
-    }
-    $('.temper-filter').val('');
-  });
-}
-
-// filter by temperament
-view.tempFilter = function(){
-  $('.temper-filter').change(function(){
-    $('article').hide();
-    var selectedValue = $(this).val();
-    console.log(selectedValue);
-    if(selectedValue){
-      $('article[data-temper="' + selectedValue + '"]').fadeIn(800);
-    }else{
-      $('article').not('.template').show();
-    }
-    $('.chicken-filter').val('');
-  });
-}
-
-view.teaser = function(){
-  console.log('in the fuction');
-  var hiddenContent = $('.description *:nth-of-type(n+2)')
-  hiddenContent.hide();
-  $('.read-on').click(function(e) {
-    e.preventDefault();
-    console.log('you clicked');
-    if($(this).text() === 'Read more') {
-      $('.description').find('p').fadeIn();
-      $(this).text('Show Less');
-    } else if ($(this).text() === 'Show Less'){
-      hiddenContent.hide();
-      $(this).text('Read more');
-    }
-  });
-}
-
-view.singlePage = function(){
-  //show only About section
-  $('nav').find('li:first').on('click', function(){
-    $('#chickens').hide();
-    $('#about').fadeIn().css({"height": "600px"});
-  });
-  //show only Chickens section
-  $('nav').find('li:nth-child(2)').on('click', function(e){
-    console.log("ide click");
-    e.preventDefault();
-    $('#chickens').show();
-    $('#about').hide();
-  });
-}
-
-view.create = function(){
-  let chicken = new Chicken({
-    title: $('#chicken-title').val(),
-    description: $('#chicken-body').val(),
-    eggProduction: $('#chicken-eggProduction').val(),
-    temperament: $('#chicken-temperament').val(),
-    startLaying: $('#chicken-startLaying').val(),
-    weight: $('#chicken-weight').val(),
-    publishedOn: new Date(),
-  });
-  $('#articles').empty();
-  $('#articles').append(chicken.toHtml());
-  console.log('ek',chicken.toHtml());
-}
-
-$('#new-form').on('change', 'input, textarea', view.create);
-view.chickFilter();
-view.tempFilter();
-view.teaser();
-view.singlePage();
