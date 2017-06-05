519146bb9abc00f57c545e4654f98dbdf12caa82
erdem koral <koral.erdem@gmail.com>
2017-06-04 23:56:47 -0700

    bug fix

--- public/scripts/models/repos.js
+++ public/scripts/models/repos.js
@@ -1,16 +1,16 @@
 'use strict';
-
+var githubToken = 'faeb8169e88e13335f540db06760adb6cd0b6f50';
 (function(module){
 const repos = {};
 repos.all = [];
-repos.requestRepos = function(callback){
+repos.requestRepos = function(callback) {
   $.ajax({
-    url: 'https://api.github.com/user/repos?type=owner',
-    method: 'GET',
-    headers: {
-      'Authorization': `token ${githubToken}`
-    }
-  })
+      url: 'https://api.github.com/user/repos?type=owner',
+      method: 'GET',
+      headers: {
+        Authorization:`token ${githubToken}`
+      }
+    })
   .then(
     data => {
       repos.all = data;
